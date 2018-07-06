5a207f6e473d26ad17af403864c5e8f95201b843
NathanSeltzer <nseltzer@wisc.edu>
2018-07-06 10:27:51 -0500

    runfile

---
+++ 00_runfile_ltmv.R
@@ -0,0 +1,17 @@
+## 00 ltmv runfile
+## "less trust, moore verification"
+## Nathan Seltzer
+
+## clean out any previous work
+paths <- c("clean data", "graphics", "output")
+
+for(path in paths){
+  unlink(path, recursive = TRUE)    # delete folder and contents
+  dir.create(path)                  # create empty folder
+}
+
+## run my scripts
+source("01_import_ltmv.R")
+source("02_prep_ltmv.R")
+source("03_plot_ltmv.R")
+rmarkdown::render("04_report_ltmv.Rmd", output_dir = "output")
